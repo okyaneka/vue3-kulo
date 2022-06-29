22f8fb1597274cd7729cd510d864cb2bb49a353c
Oky Aneka <okyaneka@gmail.com>
2022-06-29 14:53:42 +0700

    init tailwind kit

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -2,10 +2,15 @@
 require("@rushstack/eslint-patch/modern-module-resolution");
 
 module.exports = {
-  "root": true,
-  "extends": [
+  root: true,
+  extends: [
     "plugin:vue/vue3-essential",
     "eslint:recommended",
-    "@vue/eslint-config-prettier"
-  ]
-}
+    "@vue/eslint-config-prettier",
+  ],
+  env: {
+    browser: true,
+    amd: true,
+    node: true,
+  },
+};
--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,9 +1,21 @@
 <script setup>
-import TheWelcome from '@/components/TheWelcome.vue'
+import { computed } from "vue";
+
+const welcome = computed(() => "Welcome to Vue 3 + Tailwind Kulo");
 </script>
 
 <template>
-  <main>
-    <TheWelcome />
-  </main>
+  <div class="m-auto">
+    <div class="text-5xl text-center">
+      <div
+        v-for="(char, i) in welcome"
+        :key="i"
+        class="animate-pulse inline-block"
+        :class="char != ' ' ? '' : 'ml-4'"
+        :style="{ animationDelay: i * 0.05 + 's' }"
+      >
+        {{ char }}
+      </div>
+    </div>
+  </div>
 </template>
