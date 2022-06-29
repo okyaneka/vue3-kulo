e5090237aee66ea730f4cfda633c48f4cd85df8b
Kulooky <okyaneka@gmail.com>
2022-06-29 14:56:20 +0700

    init tailwind kit (#1)

---
+++ src/components/layouts/default-layout.vue
@@ -0,0 +1,30 @@
+<script setup>
+import { computed } from "vue";
+
+const appTitle = computed(() => {
+  return import.meta.env.VITE_APP_TITLE;
+});
+</script>
+
+<template>
+  <header
+    class="h-14 flex items-center bg-green-50 border-b border-green-100 px-4"
+  >
+    <div class="">
+      <span class="text-lg font-semibold">
+        {{ appTitle }}
+      </span>
+    </div>
+    <div class="ml-auto rtl:mr-auto"></div>
+  </header>
+
+  <section class="flex flex-col" style="min-height: calc(100vh - (56px * 2))">
+    <router-view />
+  </section>
+
+  <footer
+    class="h-14 flex items-center bg-green-50 border-b border-green-100 px-4"
+  >
+    <div class="text-center w-full">&copy; 2022 - {{ appTitle }}</div>
+  </footer>
+</template>
