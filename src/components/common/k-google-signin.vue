2a098407ed4245bef8e5baa1c1926005a3b1b55c
Kulooky <okyaneka@gmail.com>
2022-07-14 10:08:22 +0700

    feature google signin (#2)

---
+++ src/components/common/k-google-signin.vue
@@ -0,0 +1,39 @@
+<script setup>
+import { onMounted, onUnmounted } from "vue";
+
+const emit = defineEmits(["signed-in"]);
+const CLIENT_ID = import.meta.env.VITE_APP_G_CLIENT_ID;
+
+onMounted(() => {
+  const script = document.createElement("script");
+  script.src = "https://accounts.google.com/gsi/client";
+  script.async = true;
+  script.defer = true;
+  document.body.append(script);
+
+  window.signinCallback = function (res) {
+    emit("signed-in", res);
+  };
+});
+
+onUnmounted(() => {
+  delete window.signinCallback;
+});
+</script>
+
+<template>
+  <div
+    id="g_id_onload"
+    :data-client_id="CLIENT_ID"
+    data-callback="signinCallback"
+    data-auto_prompt="false"
+  ></div>
+  <div
+    class="g_id_signin inline-block"
+    data-type="standard"
+    data-theme="outline"
+    data-text="continue_with"
+    data-shape="rectangular"
+    data-logo_alignment="left"
+  ></div>
+</template>
